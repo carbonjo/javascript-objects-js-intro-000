a276d10170ae85ff93619a6e1a884251c914e2f3
Joaquin Carbonara <joaquin.o.carbonara@gmail.com>
2018-05-12 01:41:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, name, song) {
   playlist[name] = song
   return playlist
 }
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist.name
+  return playlist
+}
